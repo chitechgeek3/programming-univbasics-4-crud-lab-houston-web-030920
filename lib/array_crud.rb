3c9f4ec7d849b3eba3552ba0a13171352e6de1fa
Rob Lawrence <iamrobnyce@gmail.com>
2020-03-01 03:15:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,9 +17,9 @@ end
 def remove_element_from_end_of_array(array)
   array.pop()
 end
- array.shift()
+
 def remove_element_from_start_of_array(array)
-    array[index_number]
+  
 end
 
 def retrieve_element_from_index(array, index_number)
